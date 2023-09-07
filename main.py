0b1f8c6937eb519533dc868199033aadc16b3d67
Aurelien Massiot <aurelien.massiot@octo.com>
2023-09-07 11:27:33 +0200

    ajout de quelques tests

--- main.py
+++ main.py
@@ -15,10 +15,6 @@ def perform_search_in_memory():
     vector_db = VectorDBInMemory()
 
     # Insert vectors into the database
-    # vector_db.insert(1, np.array([0.2, 0.2, 0.2]))
-    # vector_db.insert(2, np.array([0.4, 0.4, 0.4]))
-    # vector_db.insert(3, np.array([0.6, 0.6, 0.6]))
-
     arrays_to_insert = [np.array([1, 2, 3]),
                         np.array([4, 5, 6]),
                         np.array([7, 8, 9])]
@@ -27,8 +23,9 @@ def perform_search_in_memory():
 
     # Search for similar vectors
     query_vector = np.array([0.15, 0.25, 0.35])
-    similar_vectors = vector_db.search(query_vector, k=2)
-    logger.info(f"Similar vectors: {similar_vectors}")
+    k_similar_vectors = 5
+    similar_vectors = vector_db.search(query_vector, k=k_similar_vectors)
+    logger.info(f"Most {k_similar_vectors} Similar vectors: {similar_vectors}")
 
     # Retrieve a specific vector by its key
     retrieved_vector = vector_db.retrieve(2)
@@ -58,4 +55,4 @@ def perform_search_sqlite():
 
 if __name__ == "__main__":
     perform_search_in_memory()
-    #perform_search_sqlite()
+    perform_search_sqlite()
---
+++ tests/test_vectordb_sqlite.py
@@ -0,0 +1,55 @@
+import shutil
+
+import numpy as np
+import pytest
+
+from simple_vector_db.vector_db import VectorDBSQLite
+
+
+@pytest.fixture
+def vector_db():
+    db = VectorDBSQLite()
+    yield db
+    db.conn.close()
+    shutil.os.remove('vector_db.db')
+
+
+def test_insert_and_retrieve(vector_db):
+    #  Given
+    vectors_to_insert = [np.array([1, 2, 3]), np.array([4, 5, 6]), np.array([7, 8, 9])]
+
+    # When
+    vector_db.insert(vectors_to_insert)
+    retrieved_index, retrieved_data = vector_db.retrieve(1)
+
+    # Then
+    assert retrieved_index == 1
+    assert np.array_equal(retrieved_data, np.array([1, 2, 3]))
+
+
+def test_search_with_a_similar_vector_should_return_an_ordered_list_of_vectors_with_first_similarity_equal_to_1(
+        vector_db):
+    #  Given
+    vectors_to_insert = [np.array([1, 2, 3]), np.array([3, 2, 1])]
+    vector_db.insert(vectors_to_insert)
+    query_vector = np.array([1, 2, 3])
+    k = 2
+
+    # When
+    similar_vectors = vector_db.search(query_vector, k)
+
+    # Then
+    assert len(similar_vectors) == k
+    assert similar_vectors[0][0] == 1
+    assert similar_vectors[0][1] == 1.0
+
+
+def test_retrieve_nonexistent_key_should_return_none(vector_db):
+    # Given
+    nonexistent_key = 100
+
+    # When
+    retrieved_vector = vector_db.retrieve(nonexistent_key)
+
+    # Then
+    assert retrieved_vector is None
