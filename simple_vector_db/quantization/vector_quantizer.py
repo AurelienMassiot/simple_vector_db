467af66c17921531fa59d85f2ae8f82e3b00b4f6
philippe.stepniewski <philippe.stepniewski@gmail.com>
2023-09-08 16:30:32 +0200

    wip: working on computing cluster in subspaces for quantization

--- simple_vector_db/quantization/vector_quantizer.py
+++ simple_vector_db/quantization/vector_quantizer.py
@@ -1,4 +1,10 @@
 import numpy as np
+import logging
+from utils.flex_logging import stream_handler
+
+logger = logging.getLogger(__name__)
+logger.addHandler(stream_handler)
+logger.setLevel(logging.INFO)
 
 
 class VectorQuantizer:
@@ -8,4 +14,12 @@ class VectorQuantizer:
         self.k_centroids = k_centroids
 
     def split_vector_into_chunks(self, input_vector: np.array):
-        return input_vector
+        vector_dimension = input_vector.shape[0]
+        if vector_dimension % self.m_chunks != 0:
+            logger.error(f"The vector's dimension {vector_dimension} is not divisible by {self.m_chunks}")
+        chunck_dimension = int(vector_dimension / self.m_chunks)
+        chunks = [input_vector[ch * chunck_dimension:(ch + 1) * chunck_dimension] for ch in range(self.m_chunks)]
+        return chunks
+
+    def compute_clusters_on_subspace(self):
+
