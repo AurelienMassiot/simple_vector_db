13cbfa291271055b1d821454a2fa70e4b2e0a308
philippe.stepniewski <philippe.stepniewski@gmail.com>
2023-09-08 17:47:06 +0200

    wip: finished function computing centroids on subspace and creating codebook

--- simple_vector_db/quantization/vector_quantizer.py
+++ simple_vector_db/quantization/vector_quantizer.py
@@ -1,6 +1,8 @@
 import numpy as np
 import logging
 from utils.flex_logging import stream_handler
+from sklearn.cluster import KMeans
+from typing import Dict
 
 logger = logging.getLogger(__name__)
 logger.addHandler(stream_handler)
@@ -12,6 +14,8 @@ class VectorQuantizer:
     def __init__(self, m_chunks: int, k_centroids: int):
         self.m_chunks = m_chunks
         self.k_centroids = k_centroids
+        self.nb_subspace_centroids = int(k_centroids / m_chunks)
+        self.codebook: Dict[str, int] = {}
 
     def split_vector_into_chunks(self, input_vector: np.array):
         vector_dimension = input_vector.shape[0]
@@ -21,5 +25,14 @@ class VectorQuantizer:
         chunks = [input_vector[ch * chunck_dimension:(ch + 1) * chunck_dimension] for ch in range(self.m_chunks)]
         return chunks
 
-    def compute_clusters_on_subspace(self):
-
+    def compute_clusters_on_subspace(self, subspace_vectors: list[np.array], subspace_index: int):
+        kmeans = KMeans(n_clusters=self.nb_subspace_centroids, random_state=0, n_init="auto")
+        kmeans.fit_predict(subspace_vectors)
+        centroids = kmeans.cluster_centers_
+        predicted_clusters = kmeans.labels_
+        predicted_clusters = predicted_clusters + (self.nb_subspace_centroids * subspace_index)
+        codebook_labels = np.array(range(0, self.nb_subspace_centroids)) + (self.nb_subspace_centroids * subspace_index)
+        logger.info(centroids)
+        for i, el in enumerate(centroids):
+            self.codebook[codebook_labels[i]] = el
+        return centroids, predicted_clusters, codebook_labels
--- tests/quantization/test_vector_quantizer.py
+++ tests/quantization/test_vector_quantizer.py
@@ -2,6 +2,13 @@ import pytest
 import numpy as np
 from simple_vector_db.quantization.vector_quantizer import VectorQuantizer
 
+sample_vectors = [
+    np.array([0.1, 0.2, 0.3]),
+    np.array([0.4, 0.5, 0.6]),
+    np.array([0.7, 0.8, 0.9]),
+    np.array([0.5, 0.7, 0.3])
+]
+
 
 @pytest.fixture
 def test_vector():
@@ -9,20 +16,79 @@ def test_vector():
     return np.array([2.0, 4.0, 2.0, 1.0, 3.0, 5.0, 3.0, 2.0])
 
 
-def test_split_vector_into_chunks_should_return_correct_chunks(test_vector):
+@pytest.fixture()
+def test_quantizer():
+    return VectorQuantizer(m_chunks=4, k_centroids=8)
+
+
+def test_split_vector_into_chunks_should_return_correct_chunks(test_vector, test_quantizer):
     # GIVEN
     expected_chunks = [[2.0, 4.0], [2.0, 1.0], [3.0, 5.0], [3.0, 2.0]]
-    quantizer = VectorQuantizer(m_chunks=4, k_centroids=4)
     # WHEN
-    chunks_result = quantizer.split_vector_into_chunks(test_vector)
+    chunks_result = test_quantizer.split_vector_into_chunks(test_vector)
     # THEN
     assert (np.array_equal(chunks_result, expected_chunks))
 
 
 def test_split_vector_into_chunks_should_return_an_error_if_divisible(test_vector, caplog):
     # GIVEN
-    quantizer = VectorQuantizer(m_chunks=5, k_centroids=4)
+    quantizer = VectorQuantizer(m_chunks=5, k_centroids=8)
     # WHEN
     chunks_result = quantizer.split_vector_into_chunks(test_vector)
     # THEN
     assert f"The vector's dimension 8 is not divisible by 5" in caplog.text
+
+
+def test_compute_clusters_on_subspace_should_return_correct_number_of_clusters(test_quantizer):
+    # GIVEN
+    expected_number_of_cluster = 2
+    assert test_quantizer.nb_subspace_centroids == expected_number_of_cluster
+    # WHEN
+    centroids, predicted_clusters, codebook_labels = test_quantizer.compute_clusters_on_subspace(sample_vectors,
+                                                                                                 subspace_index=0)
+    # THEN
+    assert len(centroids) == expected_number_of_cluster
+    assert len(set(predicted_clusters)) == expected_number_of_cluster
+
+
+def test_compute_clusters_on_subspace_should_return_correct_global_codebook_index(test_quantizer):
+    # GIVEN
+    expected_codebook_indexes_for_subspace_2 = [5, 5, 4, 5]
+    # WHEN
+    centroids, predicted_clusters, codebook_labels = test_quantizer.compute_clusters_on_subspace(sample_vectors,
+                                                                                                 subspace_index=2)
+    # THEN
+    for i, expected_el in enumerate(expected_codebook_indexes_for_subspace_2):
+        assert predicted_clusters[i] == expected_el
+
+
+def test_compute_clusters_on_subspace_should_return_correct_codebook_labels(test_quantizer):
+    # GIVEN
+    expected_codebook_labels_for_subspace_2 = [4, 5]
+    # WHEN
+    centroids, predicted_clusters, codebook_labels = test_quantizer.compute_clusters_on_subspace(sample_vectors,
+                                                                                                 subspace_index=2)
+    # THEN
+    for i, expected_el in enumerate(expected_codebook_labels_for_subspace_2):
+        assert codebook_labels[i] == expected_el
+
+
+def test_compute_clusters_should_add_to_codebook_correct_clusters_and_ids(test_quantizer):
+    # GIVEN
+    centroid_0_4 = np.array([0.7, 0.8, 0.9])
+    centroid_0_5 = np.array([0.33333333, 0.46666667, 0.4])
+    # WHEN
+    centroids, predicted_clusters, codebook_labels = test_quantizer.compute_clusters_on_subspace(sample_vectors,
+                                                                                                 subspace_index=2)
+    res_codebook = test_quantizer.codebook
+    # THEN
+    for i,exp_el in enumerate(centroid_0_4):
+        assert res_codebook[4][i] == pytest.approx(exp_el)
+
+    for i,exp_el in enumerate(centroid_0_5):
+        assert res_codebook[5][i] == pytest.approx(exp_el)
+
+
+
+if __name__ == "__main__":
+    pytest.main()
