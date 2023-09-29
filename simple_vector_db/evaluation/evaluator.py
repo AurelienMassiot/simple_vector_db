ad3959ac8c18a69a42914f984238007f9fa590cd
philippe.stepniewski <philippe.stepniewski@gmail.com>
2023-09-29 18:04:24 +0200

    feat: finished evaluator computing the recall between ANN and KNN at a given k. For now, cant reach 100% recall because of ties in the knn using cosine sim

--- simple_vector_db/evaluation/evaluator.py
+++ simple_vector_db/evaluation/evaluator.py
@@ -1,7 +1,12 @@
 import numpy as np
 from simple_vector_db.vector_db import VectorDB
 from sklearn.neighbors import NearestNeighbors
+import logging
+from utils.flex_logging import stream_handler
 
+logger = logging.getLogger(__name__)
+logger.addHandler(stream_handler)
+logger.setLevel(logging.INFO)
 
 class Dataset:
     def __init__(self, vectors, k):
@@ -20,11 +25,10 @@ class Dataset:
     def map_ids_to_brute_knn(self):
         for currid in self.ids:
             reel_knn = list(self.brute_knn.kneighbors(self.vectors[currid].reshape(1, -1))[1][0])
-            reel_knn.pop(0)
             self.ids_to_brute_knn[currid] = reel_knn
 
     def init_brute_force_knn(self):
-        brute_knn = NearestNeighbors(n_neighbors=self.k + 1, algorithm="brute")
+        brute_knn = NearestNeighbors(n_neighbors=self.k + 1, algorithm="brute", metric='cosine')
         brute_knn.fit(self.vectors)
         self.brute_knn = brute_knn
 
@@ -44,10 +48,22 @@ class VectorDBEvaluator:
     def query_with_all_vectors(self, k: int):
         results = []
         for vector in self.bench_dataset.vectors:
-            results.append(self.vector_db.search_without_index(vector, k))
+            results.append(self.vector_db.search_without_index(vector, k+1))
         return results
 
-    def compute_map_on_results(self):
-        for res in results:
-            vec_id = res[0]
-            
+    def compute_recall_on_results(self, results):
+        total_recall = 0
+        for query_id, res in enumerate(results):
+            real_knn = self.bench_dataset.ids_to_brute_knn[query_id]
+            pred_knn = []
+            for res_vec_id, sim in res:
+                pred_knn.append(res_vec_id-1)
+            real_knn.sort()
+            pred_knn.sort()
+
+            recall = len(set(real_knn).intersection(pred_knn)) / len(pred_knn)
+            if recall < 1:
+                logger.debug(real_knn, "/", pred_knn)
+                logger.debug(results[query_id])
+            total_recall += recall
+        return total_recall / len(results)
