1dd0b9aaa9c737e63c5bd04d630321cc2ca43270
philippe.stepniewski <philippe.stepniewski@gmail.com>
2023-09-29 18:04:24 +0200

    feat: finished evaluator computing the recall between ANN and KNN at a given k. For now, cant reach 100% recall because of ties in the knn using cosine sim

--- simple_vector_db/evaluation/main_evaluator.py
+++ simple_vector_db/evaluation/main_evaluator.py
@@ -5,12 +5,10 @@ from simple_vector_db.vector_db_sqlite import VectorDBSQLite
 from sklearn.datasets import load_digits
 import logging
 from utils.flex_logging import stream_handler
-import dataget
-import numpy as np
 
 logger = logging.getLogger(__name__)
 logger.addHandler(stream_handler)
-logger.setLevel(logging.DEBUG)
+logger.setLevel(logging.INFO)
 
 
 def load_mnist_data():
@@ -18,16 +16,6 @@ def load_mnist_data():
     random.shuffle(digits)
     #digits = digits[0:100]
     return digits
-
-def load_fashion_mnist(n_sample = 1000):
-    X_train, y_train, X_test, y_test = dataget.image.fashion_mnist().get()
-    X = np.vstack([X_train,X_test])
-    images = []
-    for image in X:
-        images.append(image.flatten())
-    random.shuffle(images)
-    images = images[0:n_sample]
-    return images
 
 def remove_sqlite_file():
     db_filename = "test_db.db"
@@ -40,20 +28,15 @@ def prepare_sql_db():
     db_filename = "test_db.db"
     remove_sqlite_file()
     vector_db = VectorDBSQLite(db_filename)
-    vector_db.set_metric("euclidean")
     return vector_db
 
 if __name__ == "__main__":
-    images = load_fashion_mnist(n_sample=100)
-    logger.info("Loaded Dataset")
+    digits = load_mnist_data()
     vector_db = prepare_sql_db()
-    logger.info("Initiated Vector DB")
-    k = 50
-    ds = Dataset(images, k=k)
+    k = 10
+    ds = Dataset(digits, k=k)
     eval = VectorDBEvaluator(vector_db, ds)
-    logger.info("Created evaluation Dataset")
     results = eval.query_with_all_vectors(k=k)
-    logger.info("Queried vector db with all data")
     recall = eval.compute_recall_on_results(results)
     logger.info(f"Computed RECALL @ {k} is {str(recall)}")
     remove_sqlite_file()
