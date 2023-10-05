8fbb97158a464ba40059063612ff9d4b835f5f53
philippe.stepniewski <philippe.stepniewski@gmail.com>
2023-10-05 22:40:46 +0200

    fix: corrected a bug in the numpyarrayadapter. Unbuffering should use dtype and size of numpy array. Tests added for ints and floats

--- simple_vector_db/numpy_array_adapter.py
+++ simple_vector_db/numpy_array_adapter.py
@@ -4,11 +4,15 @@ from sqlalchemy import types
 
 class NumpyArrayAdapter(types.TypeDecorator):
     impl = types.LargeBinary
+    dtype: int = None
+    count: int = None
 
     def process_bind_param(self, value, dialect):
+        self.dtype = value.dtype
+        self.count = len(value)
         if value is not None:
             return value.tobytes()
 
     def process_result_value(self, value, dialect):
         if value is not None:
-            return np.frombuffer(value, dtype=np.float64)
+            return np.frombuffer(value, dtype=self.dtype, count=self.count)
