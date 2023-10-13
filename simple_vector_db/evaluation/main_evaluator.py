4ed7c52efadd092b44146e12301a0b58062d92d9
philippe.stepniewski <philippe.stepniewski@gmail.com>
2023-10-13 19:03:34 +0200

    wip

--- simple_vector_db/evaluation/main_evaluator.py
+++ simple_vector_db/evaluation/main_evaluator.py
@@ -1,5 +1,8 @@
 import random
 import shutil
+
+import pandas as pd
+
 from simple_vector_db.evaluation.evaluator import Dataset, VectorDBEvaluator
 from simple_vector_db.vector_db_sqlite import VectorDBSQLite
 from sklearn.datasets import load_digits
@@ -7,6 +10,7 @@ import logging
 from utils.flex_logging import stream_handler
 import dataget
 import numpy as np
+import time
 
 logger = logging.getLogger(__name__)
 logger.addHandler(stream_handler)
@@ -16,19 +20,20 @@ logger.setLevel(logging.DEBUG)
 def load_mnist_data():
     digits = load_digits().data
     random.shuffle(digits)
-    #digits = digits[0:100]
+    # digits = digits[0:100]
     return digits
 
 
-def load_fashion_mnist(n_sample = 1000):
+def load_fashion_mnist(n_sample=1000):
     X_train, y_train, X_test, y_test = dataget.image.fashion_mnist().get()
-    X = np.vstack([X_train,X_test])
+    X = np.vstack([X_train, X_test])
     images = []
     for image in X:
         images.append(image.flatten())
     random.shuffle(images)
     images = images[0:n_sample]
     return images
+
 
 def remove_sqlite_file():
     db_filename = "test_db.db"
@@ -37,6 +42,7 @@ def remove_sqlite_file():
     except FileNotFoundError:
         pass
 
+
 def prepare_sql_db():
     db_filename = "test_db.db"
     remove_sqlite_file()
@@ -44,16 +50,29 @@ def prepare_sql_db():
     vector_db.set_metric("euclidean")
     return vector_db
 
+
 if __name__ == "__main__":
-    images = load_fashion_mnist(n_sample=100)
+    remove_sqlite_file()
+    images = load_fashion_mnist(n_sample=5000)
     logger.info("Loaded Dataset")
-    vector_db = prepare_sql_db()
     logger.info("Initiated Vector DB")
-    k = 50
-    ds = Dataset(images, k=k)
-    eval = VectorDBEvaluator(vector_db, ds)
-    logger.info("Created evaluation Dataset")
-    results = eval.query_with_all_vectors(k=k)
-    logger.info("Queried vector db with all data")
-    recall = eval.compute_recall_on_results(results)
-    logger.info("Recall is "+str(recall))
+    results_bench = []
+    for nb_clusters in range(2, 256):
+        vector_db = prepare_sql_db()
+        k = 50
+        ds = Dataset(images, k=k)
+        eval = VectorDBEvaluator(vector_db, ds)
+        eval.set_kmeans_index(nb_clusters)
+        logger.info("Created evaluation Dataset")
+        time_start = time.time()
+        results = eval.query_with_all_vectors_kmeans_index(k=k)
+        time_end = time.time()
+        time_total = time_end - time_start
+        logger.info("Queried vector db with all data in" + str(time_total))
+        recall = eval.compute_recall_on_results(results)
+        results_bench.append({"nb_clusters": nb_clusters, "k": k, "recall": recall, "total_time": time_total,
+                        "nb_requests_per_second": int(len(images) / time_total)})
+        logger.info("Recall is " + str(recall))
+        print(results_bench)
+    results_bench = pd.DataFrame(results_bench)
+    results_bench.to_csv("figures/bench_results_kmeans.csv")
